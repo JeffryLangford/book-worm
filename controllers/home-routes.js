11198660bbbb603d8bab60a2b1b6e1a66b1f2ecb
Jeffry Langford <jblangford97@gmail.com>
2022-06-29 18:16:16 -0500

    styled cards

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,7 +3,9 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => {
     console.log(req.session);
-    res.render('homepage', { loggedIn: req.session.loggedIn });
+    res.render('homepage', { 
+      loggedIn: req.session.loggedIn 
+    });
 });
 
 router.get('/login', (req, res) => {
