f7267c08ed46fb66ff4b35d23e4e95c9c126f3d5
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2022-06-30 13:54:43 -0500

    using axios for bestsellers api get route

--- controllers/api/book-routes.js
+++ controllers/api/book-routes.js
@@ -1,15 +1,8 @@
-const express = require('express');
 const router = require('express').Router();
 const path = require('path');
-const sequelize = require('../../config/connection');
-//const { User, Library, Index } = require('../models/');
 const axios = require("axios");
-
-const Books = require('../../models/library');
-const https = require('https');
-//const  { newValue } = require('../../public/js/library');
-// add user's parameters to url** // testing parameters 
-//let bestSellers = "https://api.nytimes.com/svc/books/v3/lists/hardcover-fiction.json?api-key=tZFDyz4NLXMBFeRo9UhOkGW5lVUo7Lr6";
+const { Choice, User, Comment, Post } = require('../../models');
+//const Books = require('../../models/library');
 
 // GET route for book recs using axios (api/books/recs) ***THIS CODE WORKS***
 router.get('/recs', async (req, res) => {
@@ -40,15 +33,15 @@ router.get('/recs', async (req, res) => {
       }
   })
 
-
 // POST route to add book recs to database(api/books/recs)*****************************
 router.post('/recs', (req, res) => {
    //res.send(req.body);
-   Books.create({
+   Choice.create({
     title: req.body.title,
     author: req.body.author,
     user_id: req.body.user_id,
-    genre: req.body.genre
+    genre: req.body.genre,
+    post_id: req.body.post_id
   })
     .then(answers => res.json(answers))
     .catch(err => {
@@ -57,11 +50,11 @@ router.post('/recs', (req, res) => {
     });
 });
 
-
 // GET route for user's books (api/books/library) ***THIS CODE WORKS*** 
+  // this route can be moved to user-routes.js
 router.get('/library', (req, res) => 
 
-  Books.findAll({
+  Choice.findAll({
     attributes: ['id',
       'user_id',
       'title',
@@ -85,18 +78,21 @@ router.get('/library', (req, res) =>
         console.log('Error :' );
     }) 
   })
-
 )
+
+/*
 // POST route for user to add a new book into database (api/books/new) ***THIS CODE WORKS***
+// THIS route is already in user routes(not needed)
     // add functionality for user to input data into form and add to database   
 router.post('/new', (req, res) => {
   
   // insert into table
-  Books.create({
+  Choice.create({
        title: req.body.title,
        genre: req.body.genre,
        author: req.body.author, 
-       user_id: req.body.user_id   
+       user_id: req.body.user_id,
+       post_id: req.body.post_id  
   })
   .then(answer => res.json(answer))
   
@@ -106,10 +102,11 @@ router.post('/new', (req, res) => {
       res.status(500).json(err);
     })
 });
+*/
 
 // DELETE route for user to delete book by id (/api/books/:id) **THIS CODE WORKS**
 router.delete('/:id', (req, res) => {
-  Books.destroy({
+  Choice.destroy({
     where: {
       user_id: req.body.user_id,
       id: req.params.id
@@ -128,62 +125,14 @@ router.delete('/:id', (req, res) => {
     });
 });
 
-/*
-// GET route for bestsellers (api/books/bestsellers) *** THIS CODE WORKS ***
-  // add book to library functionality 
-router.get('/bestsellers', (req, res) => {
-  https.get(bestSellers, (resp) => {
-    let data = '';
-    resp.on('data', chunk => { 
-        data += chunk;
-    });
-    // parse data
-    resp.on('end', () => {
-        //res.json(JSON.parse(data))
-
-        // parse data
-        const answers = JSON.parse(data);
-        console.log(data);
-        // destructure object
-        const { results:{ list_name, books}} = answers;
-        console.log(list_name);
-        
-        // create a new array of bestselling books 
-        const nyTimes = books.map(({rank, title, author, description, book_image}) => ({Rank: rank, Title: title, Author: author, Description: description, Picture: book_image}));
-        console.log(nyTimes);
-        
-  
-    // render info in bestsellers partials handlebars
-    res.render('partials/bestsellers-details',{
-      nyTimes,
-      list_name
-      //genre
-      });
-      req.on('error', ()=> {
-      console.log('Error :' );
-      });
-    })
-  });
-});
-*/
-
-// TESTING GET route using axios
+// GET route for bestsellers (api/books/bestsellers) ***THIS CODE WORKS***
 router.get('/bestsellers', async (req, res) => {
-  //const params = new URLSearchParams();
-  //const category = JSON.stringify(req.body);  
-    //params.append("q", `${category}`);
-    //const url = "https://api.nytimes.com/svc/books/v3/lists/"
-    //const params = new URLSearchParams();
-    //const category = JSON.parse(JSON.stringify(req.body));  
-    const category = JSON.stringify(req.params);
-      //params.append(`${category}`);
-      //const request = {
-       // params: params
-      //}; 
-    //const request = JSON.stringify(req.body);
-    
+   // const category  = JSON.stringify(req.body);
+   const { lists } = req.body;
+   console.log(lists);
+
       try {
-          const response = await axios.get("https://api.nytimes.com/svc/books/v3/lists/" + category + ".json?api-key=tZFDyz4NLXMBFeRo9UhOkGW5lVUo7Lr6")
+          const response = await axios.get("https://api.nytimes.com/svc/books/v3/lists/" + `${lists}`  + ".json?api-key=tZFDyz4NLXMBFeRo9UhOkGW5lVUo7Lr6")
           const answers = response.data
           //res.send(response.data);
                   // destructure object
@@ -201,16 +150,17 @@ router.get('/bestsellers', async (req, res) => {
       catch (err) {
           console.log(err)
       }
-        })
+    })
      
 // POST route to add a bestseller to user's library (api/books/bestsellers) ***THIS CODE WORKS***
 router.post('/bestsellers', (req, res) => {
   //res.send(req.body);
-  Books.create({
+  Choice.create({
     title: req.body.title,
     author: req.body.author,
     user_id: req.body.user_id,
-    genre: req.body.genre
+    genre: req.body.genre,
+    post_id: req.body.post_id
   })
     .then(answer => res.json(answer))
     .catch(err => {
@@ -218,5 +168,35 @@ router.post('/bestsellers', (req, res) => {
       res.status(500).json(err);
     });
 });
+
+// GET route for user's dashboard (api/books/dashboard) ***THIS CODE WORKS*** 
+router.get('/dashboard', async (req, res) => {
+ const results = await User.findAll({
+ 
+  attributes: {
+    exclude: ['password'],
+  },
+    include: [{
+        model: Post,
+        include: [{
+          model: Comment,
+          attributes: {
+            exclude: ['post_id', 'id'],
+          }
+        }],
+        where: {
+          user_id: 1
+        },
+        attributes: {
+          exclude: ['createdAt', 'updatedAt', 'post_id']
+        }
+        //association: "user_id",
+        //attributes: ["username", "comment_text", "title", "post_text"]
+      }
+    ]
+  });
+  return res.json(results);
+})
+ 
 
 module.exports = router;
