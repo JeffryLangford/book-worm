5894e4f38eba9d7cbb3342aebc368e604c15c133
Slimeball <alexramo2330@gmail.com>
2022-07-03 21:25:54 -0500

    search-bar

--- public/js/api-fetch.js
+++ public/js/api-fetch.js
@@ -6,10 +6,12 @@ const key = {
   }
 };
 
-const nyTimesApiCall = () => {
-    let apiSelection = `https://api.nytimes.com/svc/books/v3/lists/full-overview.json`
+let searchbar = document.getElementById('searchBook');
 
-    fetch(apiSelection).then(response => {
+const nyTimesApiCall = (choice) => {
+    let apiSelection = `https://api.nytimes.com/svc/books/v3/lists/full-overview.json?published_date=${choice}&api-key=smvg8ZcHWTc23U5oSLSWIH1yCkOWvhP9`
+
+    fetch(apiSelection, key).then(response => {
         if (response.ok) {
             response.json().then(data => {
                 displayTopBook(data);
@@ -19,5 +21,15 @@ const nyTimesApiCall = () => {
         }
     }).catch(err => console.error(err));
 }
+
+$('#bookSearch').submit(function(event){
+
+    // get the textbox value
+    var bookText = $(`#bookSearch`).val();
+    // takes the text to put in the search
+    nyTimesApiCall(`${bookText}`);
+    event.preventDefault();
+    console.log(nyTimesApiCall);
+});
 
 //const displayTopBook = () => {
